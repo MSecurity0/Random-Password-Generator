99ff55ac24badd69c3c1ddf2910c1634a481d7d9
MSecurity0 <61111442+MSecurity0@users.noreply.github.com>
2024-04-06 01:30:25 +0300

    Update algorithm.py

--- algorithm.py
+++ algorithm.py
@@ -8,22 +8,18 @@ from PIL import Image
 from customtkinter import CTkLabel
 import webbrowser
 
-sLetters = string.ascii_lowercase
-cLetters = string.ascii_uppercase
-digit = string.digits
 
 class PasswordGeneratorApp:
     def __init__(self, master):
         self.master = master
         master.geometry("600x600")
-
         master.title("Random Password Generator")
 
         self.image_path = os.path.join(os.path.dirname(__file__), "images/logo.jpg")
         self.image = ctk.CTkImage(light_image= Image.open(self.image_path),size=(150,50))
         self.image_label = CTkLabel(master=master, image=self.image,text='')
         self.image_label.pack(side='top',pady=10)
-
+        
         self.entry_length = ctk.CTkEntry(master,placeholder_text='Enter length of password (6-60)',width=200)
         self.entry_length.pack(side='top',pady=10)
 
@@ -31,10 +27,11 @@ class PasswordGeneratorApp:
         self.label_options.pack(side='top',pady=10)
 
         self.lowercase_var = tk.BooleanVar()
-        self.uppercase_var = tk.BooleanVar()
         self.lowercase_checkbox = ctk.CTkCheckBox(master,corner_radius=10, text="Lowercase [abcd]         ", variable=self.lowercase_var)
+        self.lowercase_checkbox.pack(side='top', pady=10)
+
+        self.uppercase_var = tk.BooleanVar()
         self.uppercase_checkbox = ctk.CTkCheckBox(master,corner_radius=10, text="Uppercase [ABCD]         ", variable=self.uppercase_var)
-        self.lowercase_checkbox.pack(side='top',pady=10)
         self.uppercase_checkbox.pack(side='top',pady=10)
 
         self.include_numbers_var = tk.BooleanVar()
@@ -45,9 +42,7 @@ class PasswordGeneratorApp:
         self.include_symbols_checkbox = ctk.CTkCheckBox(master,corner_radius=10, text="Include symbols [@#$!]", variable=self.include_symbols_var)
         self.include_symbols_checkbox.pack(side='top',pady=10)
 
-
-
-        self.entry_quantity = ctk.CTkEntry(master,placeholder_text='Enter length of password (1-100)',width=200)
+        self.entry_quantity = ctk.CTkEntry(master,placeholder_text='Enter quantity of password (1-100)',width=200)
         self.entry_quantity.pack(side='top',pady=10)
 
         self.generate_button = ctk.CTkButton(master,corner_radius=32, width=400,height=40,text="Generate Password(s)", command=self.generate_passwords)
@@ -66,6 +61,10 @@ class PasswordGeneratorApp:
 
 
     def generate_passwords(self):
+        sLetters = string.ascii_lowercase
+        cLetters = string.ascii_uppercase
+        digit = string.digits
+
         length = int(self.entry_length.get())
         include_lowercase = self.lowercase_var.get()
         include_uppercase = self.uppercase_var.get()
@@ -97,9 +96,11 @@ class PasswordGeneratorApp:
                 else:
                     s = "password length must be between 6 and 60"
                     passwords.append(s)
+                    break
         else:
             s = "quantity length must be between 1 and 100"
             passwords.append(s)
+
 
         password_str = "\n".join(passwords)
         messagebox.showinfo("Generated Passwords", password_str)
